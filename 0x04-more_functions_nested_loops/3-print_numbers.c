c90400affba47f3823ce96e00e8f5631a6109e0d
“iddiatah” <idaraowolabi247@gmail.com>
2022-10-23 16:12:31 -0700

    print_number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int a = 0;
+		for (a = '0'; a <= '9'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+}
