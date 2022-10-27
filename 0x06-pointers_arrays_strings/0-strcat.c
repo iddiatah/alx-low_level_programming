b2a347d71dc7a459898563ba4702fa5cb0667bc3
Idara Atainyang <iatainyang@yahoo.com>
2022-10-27 21:23:20 +0100

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,15 +6,18 @@
  * Return: new char
  */
 
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
-	
-	while (dest[index++])
-		dest_len++;
-	
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-	
+	int i;
+	int n = 0;
+
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[n + i] = src[i];
+	dest[n + i] = '\0';
+
 	return (dest);
 }
