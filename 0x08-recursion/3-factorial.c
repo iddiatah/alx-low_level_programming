ce0decadd3394ea949cf4981f6eeea984e1b86bc
Idara Atainyang <iatainyang@yahoo.com>
2022-11-03 15:50:07 +0100

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,18 +1,24 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strlen_recursion - returns length of string
- * @s: string
- * Return: integer length
+ * factorial - factorial of a given number
+ * @n: integer
+ * Return: integer factorial
  */
+
 int factorial(int n)
 {
-	int result = n;
 
-	if (n < 0)
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n < 0)
+	{
 		return (-1);
-	else if (n >= 0 && n <= 1)
+	}
+	else
+	{
 		return (1);
-	result *= factorial(n - 1);
-	return (result);
+	}
 }
