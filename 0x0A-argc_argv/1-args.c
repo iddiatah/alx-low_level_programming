290b4a15c331ae3a53ebed13123cf4526183ec58
Idara Atainyang <iatainyang@yahoo.com>
2022-11-05 00:10:36 +0100

    taskk 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the name of the program
+ * @argc: count arguments
+ * @argv: Arguments
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
